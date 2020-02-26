534212c84a400273ac7fd977006ef937bd7b8d7c
Seber <seb.ber.net@hotmail.fr>
2020-02-26 15:06:04 +0100

    Add MarsRoverImpl

--- src/main/java/com/esiea/tp4A/domain/Direction.java
+++ src/main/java/com/esiea/tp4A/domain/Direction.java
@@ -3,4 +3,16 @@ package com.esiea.tp4A.domain;
 public enum Direction {
 
     NORTH, EAST, SOUTH, WEST;
+
+    public Direction left() {
+        int leftOrdinal = ordinal() - 1;
+
+        final Direction newDirection;
+        if(leftOrdinal == -1) {
+            newDirection = WEST;
+        } else {
+            newDirection = Direction.values()[leftOrdinal];
+        }
+        return newDirection;
+    }
 }
