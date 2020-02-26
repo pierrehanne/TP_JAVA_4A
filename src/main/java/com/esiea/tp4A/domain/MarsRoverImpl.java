101659660f641caa9d062981f91d40817f8b161a
PierreHanne <pierre.hanne@outlook.fr>
2020-02-26 15:35:08 +0100

    Add MarsRover-Test init position

--- src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
+++ src/main/java/com/esiea/tp4A/domain/MarsRoverImpl.java
@@ -1,4 +1,14 @@
 package com.esiea.tp4A.domain;
 
+
 public class MarsRoverImpl implements MarsRover {
+
+    public MarsRoverImpl(int x, int y, Direction direction) {
+
+    }
+
+    @Override
+    public Position move(String command) {
+        return Position.of(0,0,Direction.NORTH);
+    }
 }
--- src/test/java/com/esiea/tp4A/domain/MarsRoverImplTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverImplTest.java
@@ -1,7 +1,14 @@
 package com.esiea.tp4A.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class MarsRoverImplTest {
+    @Test
+    void initial() {
+        MarsRoverImpl marsRoverImpl = new MarsRoverImpl(0, 0, Direction.NORTH);
+        Position position = marsRoverImpl.move("");
 
+        Assertions.assertThat(position).isEqualTo(Position.of(0, 0, Direction.NORTH));
+    }
 }
