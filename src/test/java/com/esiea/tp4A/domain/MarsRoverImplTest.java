16170092c8f1a38940b9182f941766226707787f
Seber <seb.ber.net@hotmail.fr>
2020-02-26 17:33:13 +0100

    LeftTest

--- src/test/java/com/esiea/tp4A/domain/MarsRoverImplTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverImplTest.java
@@ -3,12 +3,14 @@ package com.esiea.tp4A.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class MarsRoverImplTest {
+
     @Test
     void initial() {
         MarsRoverImpl marsRoverImpl = new MarsRoverImpl(0, 0, Direction.NORTH);
         Position position = marsRoverImpl.move("");
-
         Assertions.assertThat(position).isEqualTo(Position.of(0, 0, Direction.NORTH));
     }
 
@@ -18,4 +20,15 @@ class MarsRoverImplTest {
         Position newPosition = marsRover.move("R");
         Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 0, Direction.EAST));
     }
+
+    @Test
+    void left_direction()
+    {
+        MarsRover marsRover = new MarsRoverImpl(0,0,Direction.NORTH);
+        Position position = marsRover.move("L");
+        Assertions.assertThat(position)
+            .isEqualTo(Position.of(0,0,Direction.WEST));
+    }
+
+
 }
