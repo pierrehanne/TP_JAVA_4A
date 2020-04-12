b0259e389329acf61d88a6a5b768347cf9a8a7a3
Quangou <hoang@et.esiea.fr>
2020-04-12 19:21:33 +0200

    API terminée

--- src/main/java/com/esiea/tp4A/api/ApiController.java
+++ src/main/java/com/esiea/tp4A/api/ApiController.java
@@ -1,8 +1,6 @@
 package com.esiea.tp4A.api;
 
-import com.esiea.tp4A.domain.Direction;
-import com.esiea.tp4A.domain.MarsRoverImpl;
-import com.esiea.tp4A.domain.PlanetMapImpl;
+import com.esiea.tp4A.domain.*;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
@@ -11,29 +9,64 @@ public class ApiController {
 
     Map<Integer, MarsRoverImpl> Playerz = new HashMap<>();
     Integer Id = (1 + (int)(Math.random() * ((10 - 1) + 1))); // Nombre aléatoire entre 1 et 10
+    PlanetMapImpl planetMap = new PlanetMapImpl(null);
 
-    //Permet à un joueur de rejoindre une session et attribut à un user un id ( ici fixé à 1)
+    //Permet à un joueur de rejoindre une session et attribut à un user un id
     @GetMapping("/join")
     public String join(@RequestParam(value = "name", defaultValue = "World") String name) {
-        PlanetMapImpl planetMap = new PlanetMapImpl(null);
+
         planetMap.generateObstacles();
         MarsRoverImpl marsRover = new MarsRoverImpl(0,0, Direction.NORTH,planetMap);
-        Playerz.put(1,marsRover);
+        Playerz.put(Id,marsRover);
         System.out.println("playerz:"+Playerz); //Debug visu
-        return String.format("Hello %s! your id is %d", name ,1);
+        return String.format("Id="+Id);
     }
 
-    //Position du joueur en fonction de l'id entré
+    //Position du joueur en fonction de l'id entrée
     @GetMapping("rover/position")
     public String currentPosition (@RequestParam (value = "id" , defaultValue = "-1") Integer marsRover) {
+        if (marsRover < 0) {
+            return String.format("Wrong ID");
+        }
         return String.format("Current position of rover : " + Playerz.get(marsRover).getCurrentPosition());
     }
 
-    /*
+    //Déplacement du joueur en fonction des commandes f,b,l,r
     @GetMapping("rover/move")
     public String moving(@RequestParam (value = "id", defaultValue = "-1") Integer marsRover, @RequestParam (value = "command" , defaultValue = "f") String movement) {
-
-        return String.format("Current position of rover: " + Playerz.get(marsRover).getCurrentPosition());
+        Playerz.get(marsRover).move(movement);
+        return String.format("Current position of rover : " + Playerz.get(marsRover).getCurrentPosition());
     }
-    */
+
+    //Tir
+    @GetMapping("rover/shoot")
+    public String shoot(@RequestParam (value = "id", defaultValue = "-1") Integer marsRover) {
+        Playerz.get(marsRover).Shoot(planetMap);
+        return String.format("Fire !");
+    }
+
+    //Récupérer la portée du tir
+    @GetMapping("rover/shoot/range")
+    public String shoot_range(@RequestParam (value = "id", defaultValue = "-1") Integer marsRover) {
+        int laser_range = Playerz.get(marsRover).getLaserRange();
+        return "Laser range = "+laser_range;
+    }
+
+    //Détection position des autres joueurs dans une zone de 30*30
+    @GetMapping("rover/detection")
+    public String detection(@RequestParam (value = "id", defaultValue = "-1") Integer marsRover) {
+      Position P_area = Playerz.get(marsRover).getCurrentPosition();
+      String temp ="Position: ";
+        for (HashMap.Entry player : Playerz.entrySet()) {
+            if (player.getKey() != marsRover) {
+                Position pos =  ((MarsRoverImpl)player.getValue()).getCurrentPosition();
+                if(Math.abs(pos.getX()-P_area.getX()) <= 15 && Math.abs(pos.getY()-P_area.getY()) <= 15)
+                    temp += ((MarsRoverImpl)player.getValue()).getCurrentPosition();
+                }
+        }
+        return temp;
+    }
+
+
+
 }
